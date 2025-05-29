4dd090b0b4609a49ff0b977d80862ff9955f6c15
Nick Damiano <ndamiano100@gmail.com>
2025-05-29 18:08:52 -0400

    Initial commit. Plugin is now connected to ChatGPT

--- main.ts
+++ main.ts
@@ -1,134 +1,157 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Menu,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TAbstractFile,
+	TFolder,
+} from "obsidian";
+
+import { CharacterSchema } from "CharacterSchema";
+import OpenAI from "openai";
+import { zodTextFormat } from "openai/helpers/zod";
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface CharacterCreatorSettings {
+	api_key: string;
+	template: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: CharacterCreatorSettings = {
+	api_key: "",
+	template: "# {{Name}}\n\n**Description**: {{Description}}",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class CharacterCreatorPlugin extends Plugin {
+	settings: CharacterCreatorSettings;
+
+	client: OpenAI;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.client = new OpenAI({
+			apiKey: this.settings.api_key,
+			dangerouslyAllowBrowser: true,
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+		this.registerEvent(
+			this.app.workspace.on(
+				"file-menu",
+				(menu: Menu, file: TAbstractFile) => {
+					if (file instanceof TFolder) {
+						menu.addItem((item) =>
+							item
+								.setTitle("Generate Character Here")
+								.setIcon("dice")
+								.onClick(() => this.createCharacter(file))
+						);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
-			}
-		});
+			)
+		);
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new SettingsTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async createCharacter(targetFolder: TFolder) {
+		new Notice("Creating a chracter for you.");
+		const result = await this.generateCharacter();
+		try {
+			if (!result) {
+				new Notice("ChatGPT failed to generate a character for you.");
+				return;
+			}
+			const fileName = `${result.name}.md`;
+			const filePath = `${targetFolder.path}/${fileName}`;
+			const content = result.description;
+			await this.app.vault.create(filePath, content);
+			new Notice(
+				`Character ${result.name} created in "${targetFolder.path}"`
+			);
+		} catch (err) {
+			console.error("Failed to create character note", err);
+			new Notice("Error creating character note.");
+		}
+	}
+
+	async generateCharacter() {
+		const prompt = `You are an AI that generates fantasy characters for roleplaying games. Return only JSON.
+
+		Example format:
+		{
+		"Name": "Zariah Thornroot",
+		"Description": "A cunning herbalist who trades in secrets as often as roots."
+		}
+
+		Generate one character now:
+		`;
+
+		const response = await this.client.responses.parse({
+			model: "gpt-4o",
+			instructions:
+				"You are an AI that generates fantasy characters for roleplaying games.",
+			input: "Generate a character.",
+			text: {
+				format: zodTextFormat(CharacterSchema, "character"),
+			},
+		});
+		console.log(response);
+		return response.output_parsed;
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class SettingsTab extends PluginSettingTab {
+	plugin: CharacterCreatorPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: CharacterCreatorPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Chat GPT API Key")
+			.setDesc("In order to get this go here: ")
+			.addText((text) =>
+				text
+					.setPlaceholder("This is your API key for chatGPT")
+					.setValue(this.plugin.settings.api_key)
+					.onChange(async (value) => {
+						this.plugin.settings.api_key = value;
+						await this.plugin.saveSettings();
+					})
+			);
+		new Setting(containerEl)
+			.setName("Character Template")
+			.setDesc("Use {{Name}} and {{Description}} in your template.")
+			.addTextArea((text) =>
+				text
+					.setValue(this.plugin.settings.template)
+					.onChange(async (value) => {
+						this.plugin.settings.template = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
